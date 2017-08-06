ae0af3a2a756b092db86d7bd6ec62fda97e989d7
tlfreet <tfreet@gmail.com>
2017-08-06 18:02:29 -0400

    quiz app

---
+++ questions.js
@@ -0,0 +1,112 @@
+appState.QUESTIONS = [
+    {
+        questionId: 1,
+        questionText:'What is the capital of Kentucky?',
+        answers:{
+            a:'Frankfort',
+            b:'Lexington',
+            c:'Louisville',
+            d:'Bowling Green',
+        },
+        correctAnswer: 'a',
+    },
+        {
+        questionId: 2,
+        questionText:'Bismarck is the capital of what state?',
+        answers:{
+            a:'Wyoming',
+            b:'Ohio', 
+            c:'North Dakota',
+            d:'Maryland',
+        },
+        correctAnswer: 'c',
+    },
+        {
+        questionId: 3,
+        questionText:'Which is not a state capital?',
+        answers:{
+            a:'New York City',
+            b:'Montgomery',
+            c:'Sacramento',
+            d:'Boise',
+        },
+        correctAnswer:'a',
+    },
+        {
+        questionId: 4,
+        questionText:'What is the capital of  Washington?',
+        answers:{
+            a:'Seattle',
+            b:'Forks', 
+            c:'Olympia',
+            d:'Spokane',
+        },
+        correctAnswer:'c',
+    },
+        {
+        questionId: 5,
+        questionText:'What is the capital of North Carolina?',
+        answers:{
+            a:'Charlotte',
+            b:'Raleigh', 
+            c:'Durham',
+            d:'Greensboro',
+        },
+        correctAnswer:'b',
+    },
+        {
+        questionId: 6,
+        questionText:'Which is not a state capital?',
+        answers:{
+            a:'Tallahassee',
+            b:'St. Louis', 
+            c:'Columbus',
+            d:'Harrisburg',
+        },
+        correctAnswer:'b',
+    },
+        {
+        questionId: 7,
+        questionText:'Jefferson City is the capital of what state?',
+        answers:{
+            a:'Nebraska',
+            b:'West Virginia', 
+            c:'Colorado',
+            d:'Missouri',
+        },
+        correctAnswer:'d',
+    },
+        {
+        questionId: 8,
+        questionText:'Montpelier is the capital of what state?',
+        answers:{
+            a:'Vermont',
+            b:'Virginia',
+            c:'New Jersey',
+            d:'Maine',
+        },
+        correctAnswer:'a',
+    },
+        {
+        questionId: 9,
+        questionText:'What is the capital of Minnesota?',
+        answers:{
+            a:'Saint Paul',
+            b:'St. Cloud',
+            c:'Duluth',
+            d:'Minneapolis',
+        },
+        correctAnswer:'a',
+    },
+        {
+        questionId: 10,
+        questionText:'Which is not a state capital?',
+        answers:{
+            a:'Madison',
+            b:'Pierre',
+            c:'Nashville',
+            d:'Chicago',
+        },
+        correctAnswer:'d',
+    },
+]
